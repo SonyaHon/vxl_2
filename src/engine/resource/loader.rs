25f5460d9a5eb4d687a45b522be2aebfb4472087
pvolkov <pvolkov@delimobil.ru>
2020-09-11 19:29:28 +0300

    temp

--- src/engine/resource/loader.rs
+++ src/engine/resource/loader.rs
@@ -19,11 +19,14 @@ impl Loader {
         }
     }
 
-    pub fn load_file_as_cstr(&self, asset_path: &str) -> Result<&CStr, Error> {
-        let mut file = std::fs::File::open(self.root_path.join(asset_path))?;
-
+    pub fn load_file_as_cstring(&self, asset_path: &str) -> Result<&CString, Error> {
+        let mut file = std::fs::File::open(self.root_path.join(asset_path)).unwrap();
         let mut buffer: Vec<u8> = Vec::with_capacity(file.metadata()?.len() as usize + 1);
 
-        file.read_to_end(&mut buffer);
+        if buffer.iter().find(|i| **i == 0).is_some() {
+            return Err(Error);
+        }
+
+        Ok(unsafe { ffi::CString::from_vec_unchecked(buffer) })
     }
 }
